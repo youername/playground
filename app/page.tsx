ac9ca95d8011cb5873abf282c6ac6a1072f2a335
chan <a9233892@gmail.com>
2024-10-12 18:51:24 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={28}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
